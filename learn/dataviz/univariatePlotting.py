4cf7a726751ad67db87e04e9f0a3f4ea7fda7f3b
chrolss <chrolss@kth.se>
2018-09-20 17:03:31 +0200

    first steps

--- learn/dataviz/univariatePlotting.py
+++ learn/dataviz/univariatePlotting.py
@@ -1,3 +1,9 @@
 import pandas as pd
-filepath = "../input/wine_reviews/winemag-data_first150k.csv"
-reviews = pd.read_csv(filepath, index_col=0)
+import matplotlib as plt
+
+filepath = '../input/winemag-data-130k-v2.csv'
+df = pd.read_csv(filepath)
+
+df2 = pd.DataFrame({'price' : [1,2,3], 'points' : [4,5,6]})
+df2.plot.scatter(x='price', y='points')
+

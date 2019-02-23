6bfe7997a9674fc77aea6e2cd5d351f2524e17c5
chrolss <chrolss@kth.se@>
2019-02-23 10:16:55 +0100

    Starting with training on heart disease dataset

--- competition/ML-with-a-heart/heart.py
+++ competition/ML-with-a-heart/heart.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
-import xgboost as xgb
 from sklearn.metrics import mean_squared_error
 from sklearn import svm
 from sklearn.linear_model import LogisticRegression
+from keras import Sequential
 
 # Create ECDP (Emperical cumulative distribution function)
 def ecdf(data):
@@ -34,15 +34,15 @@ y = pd.read_csv(train_labels)
 df = pd.concat([y.heart_disease_present,X],axis=1,join='inner')
 
 # Label Encoding
+X.thal = X['thal'].astype('category')
+X = pd.concat([X, pd.get_dummies(X.thal,'thal')],axis=1)
+X = X.drop(['thal'],axis=1)
 
-df.thal = df['thal'].astype('category')
-df = pd.concat([df, pd.get_dummies(df.thal,'thal')],axis=1)
-df = df.drop(['thal'],axis=1)
 # Perform Visual EDA
-_ = sns.boxplot(x='heart_disease_present',y='resting_blood_pressure',data=df)
+_ = sns.boxplot(x='heart_disease_present',y='resting_blood_pressure',data=pd.concat([y.heart_disease_present,X],axis=1,join='inner'))
+_ = sns.boxplot(x='heart_disease_present',y='serum_cholesterol_mg_per_dl',data=pd.concat([y.heart_disease_present,X],axis=1,join='inner'))
 
 # Perform correlation analysis
-
 sns.set(style='white')
 corr = df.corr()
 mask = np.zeros_like(corr, dtype=np.bool)
@@ -52,5 +52,9 @@ cmap = sns.diverging_palette(220, 10, as_cmap=True)
 sns.heatmap(corr, mask=mask,cmap=cmap,vmax=.3,center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5})
 
 # Build model
+patient_id = X.patient_id
+X_train, X_test, y_train, y_test = train_test_split(X.drop(['patient_id'], axis=1),y.drop(['patient_id'], axis=1))
 
-X_train, y_train, X_test, y_test = train_test_split(df.drop(['heart_disease_present'],axis=1),df.heart_disease_present)
+# Setup neural network
+
+model = Sequential()

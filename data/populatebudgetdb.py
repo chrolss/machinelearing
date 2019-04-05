0e4d9aae68b9e800c30101f099e1bacf05263408
chrolss <christian.olsson01@gmail.com>
2019-04-05 17:37:36 +0300

    More fun files!

--- data/populatebudgetdb.py
+++ data/populatebudgetdb.py
@@ -3,9 +3,12 @@ import pandas as pd
 
 ## Open and import Exceö file data
 
-excelfilepath = 'C:/BluePrism/budget.xlsx'
+excelfilepath = 'C://BluePrism/budget.xlsx'
 df = pd.read_excel(excelfilepath)
 
+## Clean and edit the data
 
-##
+
+
+## Push dataframe to SQL database
 

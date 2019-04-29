931dec2dbd48a227823dbad7ca8071a895777ed9
chrolss <christian.olsson01@gmail.com>
2019-04-29 22:08:54 +0200

    building an enourmous bokeh plot function for VDE

--- competition/waterpumps/waterpumps.py
+++ competition/waterpumps/waterpumps.py
@@ -3,35 +3,119 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from sklearn.cluster import KMeans
+import bokeh.plotting
+from bokeh.plotting import output_file, figure, show
 
-## Load the raw data
+# Load the raw data
 train_lab_filepath = 'competition/waterpumps/input/training_set_labels.csv'
 train_values_filepath = 'competition/waterpumps/input/training_set_values.csv'
 
 labels = pd.read_csv(train_lab_filepath)
 df = pd.read_csv(train_values_filepath)
 
-## Clean the data
+# Clean the data
 df = df.drop(columns='recorded_by', axis=0)  # not needed since all entries are identical
 
 
-## Visual data exploration
+# Visual data exploration
 vde = pd.merge(labels, df)
 
-_ = sns.countplot(x='status_group', hue='funder', data=vde)  # Plot the different status and management groups
-_ = sns.countplot(x='status_group', hue='waterpoint_type', data=vde[vde.status_group == 'non functional'])  # Plot the different management groups for non functional ones
-_ = sns.countplot(x='status_group', hue='water_quality', data=vde[vde.status_group == 'non functional'])  # Plot the different management groups for non functional ones
+# Plot the different management groups for non functional ones
+_ = sns.countplot(x='status_group', hue='waterpoint_type', data=vde[vde.status_group == 'non functional'])
+
+# Plot the different management groups for non functional ones
+_ = sns.countplot(x='status_group', hue='water_quality', data=vde[vde.status_group == 'non functional'])
+
+# Plot the map of all waterpumps
 _ = sns.scatterplot(x='longitude', y='latitude', hue='status_group', data=vde)
 
+# Bokeh plot for visual inspections and storytelling
 
-## Visual comparison of functional vs. non-functional for different features
+test = vde.status_group.unique()
+
+# Create the figure: p
+p = figure(x_range=test, x_axis_label='status group', y_axis_label='count')
+
+# Add a blue circle glyph to the figure p
+p.vbar(x=test, top=[2, 4, 6], width=0.5, color='blue')
+
+# Specify the name of the file
+output_file('bokeh_vde_waterpumps.html')
+
+# Display the plot
+show(p)
+
+from bokeh.io import show, output_file
+from bokeh.models import ColumnDataSource, FactorRange
+from bokeh.plotting import figure
+
+output_file("bars.html")
+
+# fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
+status_groups = vde.status_group.unique()
+
+#years = ['2015', '2016', '2017']
+funders = vde.funder.unique()
+
+
+def create_bokeh_dict(_df, itervar):
+    bokeh_dict = {'status_group': _df.status_group.unique()}
+    for item in _df[itervar].unique():
+        bokeh_dict[item] = [_df[itervar][(_df[itervar] == item) & (_df.status_group == 'functional')].count(),
+                            _df[itervar][(_df[itervar] == item) & (_df.status_group == 'non functional')].count(),
+                            _df[itervar][(_df[itervar] == item) & (_df.status_group == 'functional needs repair')]
+                            .count()]
+
+    return bokeh_dict
+
+plot_dict = create_bokeh_dict(vde, 'funder')
+
+status = vde.status_group.unique()
+funder = vde.funder.unique()
+
+x = [ (status, funder) for stat in status for fund in funder ]
+count = sum(zip(plot_dict['2015'], plot_dict['2016'], plot_dict['2017']), ()) # like an hstack
+
+#data = {'fruits' : fruits,
+#        '2015'   : [2, 1, 4, 3, 2, 4],
+#        '2016'   : [5, 3, 3, 2, 4, 6],
+#        '2017'   : [3, 2, 4, 4, 5, 3]}
+
+# this creates [ ("Apples", "2015"), ("Apples", "2016"), ("Apples", "2017"), ("Pears", "2015), ... ]
+x = [ (fruit, year) for fruit in fruits for year in years ]
+counts = sum(zip(data['2015'], data['2016'], data['2017']), ()) # like an hstack
+
+source = ColumnDataSource(data=dict(x=x, counts=counts))
+
+p = figure(x_range=FactorRange(*x), plot_height=250, title="Fruit Counts by Year",
+           toolbar_location=None, tools="")
+
+p.vbar(x='x', top='counts', width=0.9, source=source)
+
+p.y_range.start = 0
+p.x_range.range_padding = 0.1
+p.xaxis.major_label_orientation = 1
+p.xgrid.grid_line_color = None
+
+show(p)
+
+
+
+
+
+
+
+
+
+
+# Visual comparison of functional vs. non-functional for different features
 hue_value = 'payment'
 _ = plt.subplot(1,2,1)
 _ = sns.countplot(x='status_group', hue=hue_value, data=vde[vde.status_group == 'non functional'])  # Plot the different management groups for non functional ones
 _ = plt.subplot(1,2,2)
 _ = sns.countplot(x='status_group', hue=hue_value, data=vde[vde.status_group == 'functional'])  # Plot the different management groups for non functional ones
 
-## Correlation analysis
+# Correlation analysis
 
 X = pd.concat([vde, pd.get_dummies(vde.status_group, 'status')], axis=1)
 X = X.corr()
@@ -47,13 +131,13 @@ sns.heatmap(X, mask=mask, cmap=cmap, vmax=.3, center=0, square=True, linewidths=
 # is geographically located, the more functional it seems to be.
 # There appears to be specific regions and districts that are more affected by non-functioning pumps
 
-## Funder data
+# Funder data
 bigFund = vde['funder'].value_counts() > 10
 bigFund = bigFund[bigFund == True]
 smallFund = vde['funder'].value_counts() < 11
 smallFund = smallFund[smallFund == True]
 
-## Add new feature
+# Add new feature
 bigfunder = vde.funder.apply(lambda x: 1 if x in bigFund else 0)
 fdf = pd.DataFrame(bigfunder)
 vde = pd.merge(vde, fdf)
@@ -61,7 +145,7 @@ vde = pd.concat([vde, fdf], axis=1)
 
 _ = sns.countplot(x='status_group', hue='bigfunder', data=vde)
 
-## Kmeans analysis
+# Kmeans analysis
 
 model = KMeans(n_clusters=3)
 

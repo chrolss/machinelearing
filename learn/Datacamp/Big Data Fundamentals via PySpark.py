ce3183f38c9f63657f9cc472082c61e15663b4a1
chrolss <chrolss@kth.se@>
2019-06-28 11:45:11 +0300

    Completed NLP Fundamentals

---
+++ learn/Datacamp/Big Data Fundamentals via PySpark.py
@@ -0,0 +1,5 @@
+### A course on big data via PySpark
+# Big data is defined by its "three Vs"
+# Volume (how much data?), variety (how many different data sources?) and velocity (how fast can we
+# access the data?)
+
